6e98889a1e3f60618315751018306e2ae51b471b
aicherish0320 <aicherish0320@163.com>
2022-07-22 08:26:21 +0800

    属性定义和 h 函数

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp, defineComponent, h } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
-createApp(App).mount("#app");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const img = require('./assets/logo.png')
+
+// import App from "./App.vue"
+const App = defineComponent({
+  render() {
+    return h('div', { id: '#app' }, [
+      h('img', { src: img }),
+      h(HelloWorld, { msg: 'Hello', age: 12 })
+    ])
+  }
+})
+
+createApp(App).mount('#app')
