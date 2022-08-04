204db445648895e7729513d12c0c37f36bd627ad
aicherish0320 <aicherish0320@163.com>
2022-08-04 22:19:36 +0800

    NumberField

---
+++ lib/fields/NumberField.tsx
@@ -0,0 +1,10 @@
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'NumberField',
+  setup() {
+    return () => {
+      return <h1>Number Field</h1>
+    }
+  }
+})
---
+++ lib/fields/StringField.tsx
@@ -0,0 +1,10 @@
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'StringField',
+  setup() {
+    return () => {
+      return <h1>String Field</h1>
+    }
+  }
+})
