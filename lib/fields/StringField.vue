bb4e492f236051076c83a249225087c8ee80ac3d
aicherish0320 <aicherish0320@163.com>
2022-08-08 16:07:09 +0800

    vue 文件

---
+++ lib/fields/StringField.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" :value="value" @input="handleChange" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Schema } from '../../lib/types'
+
+// eslint-disable-next-line no-undef
+const props = defineProps<{
+  schema: Schema
+  value: string
+  onChange: (v: any) => void
+}>()
+
+const handleChange = (e: any) => {
+  console.log('e >>> ', e)
+  props.onChange(e.target.value)
+}
+</script>
+
+<style scoped></style>
